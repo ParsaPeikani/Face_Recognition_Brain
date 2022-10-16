79a65055ae8ad0792cda786730aca05774ba7929
Parsa <parsapeikani05@gmail.com>
2022-10-16 16:09:04 -0700

    linking the Clarifai Face detection with the react project to detect only one face in a picture that has multiple faces

--- src/App.js
+++ src/App.js
@@ -13,7 +13,25 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -21,8 +39,70 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-  }
+    this.setState({ imageUrl: this.state.input });
+    const USER_ID = "weekenend";
+    const PAT = "58d3e91ad2864f79bdddbe46e732c987";
+    const APP_ID = "Ali2002";
+
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+    const IMAGE_URL = this.state.input;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+      MODEL_ID +
+      "/versions/" +
+      MODEL_VERSION_ID +
+      "/outputs",
+      requestOptions
+    )
+    .then((response) => response.json())
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch((error) => console.log("error", error));
+    
+      // .then((result) => {
+      //   if (result) {
+      //     fetch('https://magicfacedetectorbackend.herokuapp.com/image', {
+      //       method: 'put',
+      //       headers: { 'Content-Type': 'application/json' },
+      //       body: JSON.stringify({
+      //         id: this.state.user.id
+      //       })
+      //     })
+      //       .then(response => response.json())
+      //       .then(count => {
+      //         this.setState(Object.assign(this.state.user, { entries: count.entries }))
+      //       })
+      //   }
+      //   this.displayFaces(this.calculateFaceLocation(result))
+      // })
+  };
+
   render() {
     return (
       <div className="App">
@@ -34,7 +114,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
 
       </div>
     );
