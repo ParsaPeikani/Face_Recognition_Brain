9214d41e2e4eed299b802a0fe2b5ed78b2063333
Parsa <parsapeikani05@gmail.com>
2022-10-19 00:29:21 -0700

    Styling the ImageLinkForm and changing the css a little bit

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,16 +5,18 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div >
         <p className='f3' style={{ color: 'yellow'}}>
-            {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+            {'This Magic Brain will detect faces in your pictures. Give it a try'}
         </p>
         <div className='center'>
-            <div className='form center pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+            <article className="br3 ba b--black-10 mv4 w-25-l mw6 shadow-5" style={{width: "5000px"}}>
+              <main className="pa4 black-80 center">
+                <input className='b pa3 input-reset ba bg-transparent w-100 yellow' type='text' onChange={onInputChange}/>
                 <button 
-                  className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+                  className='w-30 grow f4 link ph3 pv2 dib yellow b--yellow bg-transparent'
                   onClick={onButtonSubmit}
                   >Detect</button>
-            </div>
+              </main>
+            </article>
         </div>
     </div>
   );
