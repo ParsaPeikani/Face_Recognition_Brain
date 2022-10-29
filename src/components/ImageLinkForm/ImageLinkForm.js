12d3ca83d07bd2c4cda7fd80e84e474625c80471
Parsa <parsapeikani05@gmail.com>
2022-10-28 20:40:19 -0700

    Changing the logo from brain to spider, and adding a little css to the register form

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div >
         <p className='f3' style={{ color: 'yellow'}}>
-            {'This Magic Brain will detect faces in your pictures. Give it a try'}
+            {'This Magic Spider will detect faces in your pictures. Give it a try'}
         </p>
         <div className='center'>
             <article className="br3 ba b--black-10 mv4 w-25-l mw6 shadow-5" style={{width: "5000px"}}>
