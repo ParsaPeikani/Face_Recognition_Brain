010d7a92e47be689282d5faf5950b55d2e131401
Parsa <parsapeikani05@gmail.com>
2022-11-05 17:27:30 -0700

    Adding the fron end of our project to Heroku

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ class App extends Component {
     .then((response) => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://salty-retreat-22414.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://salty-retreat-22414.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
