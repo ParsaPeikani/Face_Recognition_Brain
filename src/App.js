d6b7f603c15373a59cc630bc9cf1676928385864
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-01-10 00:38:50 -0800

    Adding server

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://smartspiderserver2-com.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
