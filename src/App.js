ca3acb59d962d2f55502351642fab59319708871
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-03-10 13:43:59 -0800

    Temporary testing for the rank component

--- src/App.js
+++ src/App.js
@@ -135,12 +135,7 @@ class App extends Component {
           })
             .then((response) => response.json())
             .then((count) => {
-              this.setState({
-                user: {
-                  ...this.state.user,
-                  entries: count,
-                },
-              });
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
             .catch(console.log);
         }
